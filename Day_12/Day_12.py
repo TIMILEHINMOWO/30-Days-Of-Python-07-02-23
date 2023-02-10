57172c77845ad8756801b77261a467defaf67c9f
TIMILEHINMOWO <104873365+TIMILEHINMOWO@users.noreply.github.com>
2023-02-10 23:32:27 +0000

    Update Day_12.py

--- Day_12/Day_12.py
+++ Day_12/Day_12.py
@@ -1,5 +1,19 @@
+# Day 12
+# Shuffle list
+
 import random
 
 list = [1,2,3,4,5]
-random.shuffle(list)
-print(list)
+def shuffle_list(l):
+    random.shuffle(l)
+    return list
+
+print(shuffle_list(list))
+
+# Array of 7 unique randomly generated numbers
+
+def rand_num():
+    randomlist = random.sample(range(0, 9), 7)
+    print(randomlist)
+            
+rand_num()
